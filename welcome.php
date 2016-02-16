01ee1d923df8e4cac33d84150b6c0b99ffab8ba7
ProjectHTML <salehsonn@gmail.com>
2016-02-16 21:57:37 +0100

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!--Head Tags-->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Habbo</title>
+		
+		<!--Head Style Tags-->
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="stylesheet" href="/style/body.css">
+		<link rel="stylesheet" href="/style/welcome.css">
+		
+		<!--Head Script Tags-->
+		<script type="text/javascript" src="/script/JQuery_288028b5205d0143c0649240b8c04c3b562fb628.js"></script>
+		<script type="text/javascript" src="/script/body.js"></script>
+		<script type="text/javascript" src="/script/welcome.js"></script>
+	</head>
+	<body>
+		<!--Welcome Room-->
+		<div class="welcome_room">
+			<div id="word_1">
+				<center>
+					<img src="http://www.habborator.org/exec/speech/bubble.php?name=Frank&text=Hello%20there!%20I%20am%20Frank%20and%20today%20I%20will%20help%20you.&format=say">
+					<br />
+					<img src="http://www.habborator.org/exec/speech/bubble.php?name=Frank&text=To%20enter%20Project.%20HTML..%20Please%20say%20if%20you're%20registrated%20or%20not.&format=say">
+					<br />
+					<img src="http://www.habborator.org/exec/speech/bubble.php?text=Write%20'registrated'%20or%20'new'.&format=whisper">
+				</center>
+			</div>
+		</div>
+	</body>
+</html>
