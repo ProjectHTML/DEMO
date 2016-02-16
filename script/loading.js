f9a2e543093d02e16ddc5be1d1a9e60d3bcf2442
ProjectHTML <salehsonn@gmail.com>
2016-02-16 20:08:18 +0100

    Create loading.js

---
+++ script/loading.js
@@ -0,0 +1,76 @@
+/* Right Side Loading */
+.loading_screen_left
+{
+	background-color: #006699;
+	width: 50%;
+	height: 100%;
+	left: 0%;
+	right: 50%;
+	top: 0%;
+	bottom: 0%;
+	position: absolute;
+}
+
+#loading_line_left
+{
+	width: 6px;
+	height: 100%;
+	background-repeat: repeat-y;
+	right: -0%;
+	top: 0%;
+	bottom: auto;
+	position: absolute;
+	background-image: url(/style/loading_middle_left.png);
+}
+
+#loading_Project
+{
+	z-index: 1;
+	width: 148px;
+	height: 42px;
+	top: 0;
+	margin: auto;
+	right: -0%;
+	bottom: 0;
+	position: absolute;
+	background-image: url(/style/loading_Project.png);
+}
+
+
+/* Left Side Loading */
+.loading_screen_right
+{
+	background-color: #006699;
+	width: 50%;
+	height: 100%;
+	left: 50%;
+	right: 0%;
+	top: 0%;
+	bottom: 0%;
+	position: absolute;
+}
+
+#loading_line_right
+{
+	width: 6px;
+	height: 100%;
+	background-repeat: repeat-y;
+	left: -0%;
+	top: 0%;
+	bottom: auto;
+	position: absolute;
+	background-image: url(/style/loading_middle_right.png);
+}
+
+#loading_HTML
+{
+	z-index: 1;
+	width: 98px;
+	height: 42px;
+	top: 0;
+	margin: auto;
+	left: -0%;
+	bottom: 0;
+	position: absolute;
+	background-image: url(/style/loading_HTML.png);
+}
